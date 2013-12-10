853bbd224357e8a41fc3f3326e7040ac8bb259d5
Ronald Mannak <ronaldmannak@me.com>
2013-12-09 22:20:42 -0800

    Initial Commit

---
+++ GoldenRatioRandomColor/REMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  REMAppDelegate.h
+//  GoldenRatioRandomColor
+//
+//  Created by Ronald Mannak on 12/9/13.
+//  Copyright (c) 2013 Ronald Mannak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface REMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoldenRatioRandomColor/REMViewController.m
@@ -0,0 +1,29 @@
+//
+//  REMViewController.m
+//  GoldenRatioRandomColor
+//
+//  Created by Ronald Mannak on 12/9/13.
+//  Copyright (c) 2013 Ronald Mannak. All rights reserved.
+//
+
+#import "REMViewController.h"
+
+@interface REMViewController ()
+
+@end
+
+@implementation REMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
